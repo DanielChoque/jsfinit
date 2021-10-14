322f3a78bed21ff78ca3d575786501cb9eb3805a
DanielChoque <patzidaniel@gmail.com>
2021-10-14 16:32:33 -0400

    mp4 ajustado

--- src/main/java/com/sakadream/jsf/controller/FileUploadView.java
+++ src/main/java/com/sakadream/jsf/controller/FileUploadView.java
@@ -1,6 +1,7 @@
 package com.sakadream.jsf.controller;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -14,20 +15,34 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.NamedEvent;
 
-import org.primefaces.PrimeFaces;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.primefaces.model.UploadedFile;
+
+/*import org.primefaces.PrimeFaces;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.event.FilesUploadEvent;
 import org.primefaces.model.file.UploadedFile;
 import org.primefaces.model.file.UploadedFiles;
-import org.primefaces.util.EscapeUtils;
+import org.primefaces.util.EscapeUtils;*/
 
 @ManagedBean
 @NamedEvent
 @RequestScoped
 public class FileUploadView {
 	private final String urltes="http://10.1.26.93:39384/test/listar";
-	private UploadedFile file;
-    private UploadedFiles files;
+	//private UploadedFile file;
+    //private UploadedFiles files;
+    //private String dropZoneText = "Drop zone p:inputTextarea demo.";
+    
+    private UploadedFile file;
+    //private UploadedFiles files;
     private String dropZoneText = "Drop zone p:inputTextarea demo.";
 
     public void upload() {
@@ -35,9 +50,153 @@ public class FileUploadView {
             FacesMessage message = new FacesMessage("Successful", file.getFileName() + " is uploaded.");
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
+        
+        String output2="";
+		try {
+	         
+	        URL url = new URL("http://10.1.26.162:8090/uploadMultipleFiles");
+	        //URL url=new URL("http://localhost/pru/u.php");
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setDoOutput(true);
+	        conn.setRequestMethod("POST");
+	        conn.setRequestProperty("Content-Type", "multipart/form-data");
+	 
+	        String input = "";/*"{ \"nit\" : \"\", \"login\" : \""
+	        		+ nombreS
+	        		+ "\", \"password\" : \""
+	        		+ passwordS
+	        		+ "\", \"tipoUsuarioId\": 861}";*/
+	 
+    
+    
+    
+	        System.out.println("01 Before  from Server .... \n");
+	       OutputStream os = conn.getOutputStream();
+	        byte[] bytes = file.getContents();
+	        os.write(bytes);
+	        os.flush();
+	        System.out.println("get set:" + conn.getResponseCode());
+	 
+	        if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+	            throw new RuntimeException("Failed : HTTP error code : "
+	                + conn.getResponseCode());
+	        }
+	 
+	        BufferedReader br = new BufferedReader(new InputStreamReader(
+	                (conn.getInputStream()), "UTF-8"));
+	 
+	        String output="";
+	        System.out.println("Output from Server .... \n");
+	        while ((output = br.readLine()) != null) {
+	        	output2=output;
+	            System.out.println("output:"+output);
+	        }
+	 
+	        conn.disconnect();
+	        
+	        System.out.println("output2:" +output2);
+	       // token(output2);
+	        
+	 
+	      } catch (MalformedURLException e) {
+	 
+	        e.printStackTrace();
+	 
+	      } catch (IOException e) {
+	 
+	        e.printStackTrace();
+	 
+	     }
+    	    	
+    	System.out.println("sen video");    	
+    
+    }
+   
+    public void upoone() {
+    	//byte[] bytes = file.getContents();
+    	HttpEntity entity = MultipartEntityBuilder.create()
+                .addPart("file", new FileBody(new File("d:\\Recurso2.png")))
+                .build();
+
+		HttpPost request = new HttpPost("http://10.1.26.162:8090/uploadMultipleFiles");
+		request.setEntity(entity);
+		
+		HttpClient client = HttpClientBuilder.create().build();
+		try {
+			HttpResponse response = client.execute(request);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    
+    
+/*//////////////////////////////////*/
+    /*public void uploadMultiple() {
+        if (files != null) {
+            for (UploadedFile f : files.getFiles()) {
+                FacesMessage message = new FacesMessage("Successful", f.getFileName() + " is uploaded.");
+                FacesContext.getCurrentInstance().addMessage(null, message);
+            }
+        }
+    }
+*/
+   /* public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage message = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }*/
+
+    /*public void handleFileUploadTextarea(FileUploadEvent event) {
+        String jsVal = "PF('textarea').jq.val";
+        String fileName = EscapeUtils.forJavaScript(event.getFile().getFileName());
+        PrimeFaces.current().executeScript(jsVal + "(" + jsVal + "() + '\\n\\n" + fileName + " uploaded.')");
+    }*/
+
+    /*public void handleFilesUpload(FilesUploadEvent event) {
+        for (UploadedFile f : event.getFiles().getFiles()) {
+            FacesMessage message = new FacesMessage("Successful", f.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }*/
+
+    public UploadedFile getFile() {
+        return file;
     }
 
-    public void uploadMultiple() {
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    /*public UploadedFiles getFiles() {
+        return files;
+    }*/
+
+    /*public void setFiles(UploadedFiles files) {
+        this.files = files;
+    }*/
+
+    public String getDropZoneText() {
+        return dropZoneText;
+    }
+
+    public void setDropZoneText(String dropZoneText) {
+        this.dropZoneText = dropZoneText;
+    }
+
+    
+
+   /* public void upload() {
+        if (file != null) {
+            FacesMessage message = new FacesMessage("Successful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+*/
+    /*public void uploadMultiple() {
     	
     	
         if (files != null) {
@@ -48,22 +207,22 @@ public class FileUploadView {
             }
             
         }
-    }
+    }*/
     
     
 
-    public void handleFileUpload(FileUploadEvent event) {
+    /*public void handleFileUpload(FileUploadEvent event) {
         FacesMessage message = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
         FacesContext.getCurrentInstance().addMessage(null, message);
-    }
+    }*/
 
-    public void handleFileUploadTextarea(FileUploadEvent event) {
+    /*public void handleFileUploadTextarea(FileUploadEvent event) {
         String jsVal = "PF('textarea').jq.val";
         String fileName = EscapeUtils.forJavaScript(event.getFile().getFileName());
         PrimeFaces.current().executeScript(jsVal + "(" + jsVal + "() + '\\n\\n" + fileName + " uploaded.')");
-    }
+    }*/
 
-    public void handleFilesUpload(FilesUploadEvent event) {
+   /* public void handleFilesUpload(FilesUploadEvent event) {
         for (UploadedFile f : event.getFiles().getFiles()) {
             FacesMessage message = new FacesMessage("Successful", f.getFileName() + " is uploaded.");
             FacesContext.getCurrentInstance().addMessage(null, message);
@@ -93,8 +252,8 @@ public class FileUploadView {
     public void setDropZoneText(String dropZoneText) {
         this.dropZoneText = dropZoneText;
     }
-    
-    public void sendUrl(UploadedFile file) {
+    */
+    /*public void sendUrl(UploadedFile file) {
     	
     	String output2="";
 		try {
@@ -112,7 +271,11 @@ public class FileUploadView {
 	        		+ passwordS
 	        		+ "\", \"tipoUsuarioId\": 861}";*/
 	 
-	        OutputStream os = conn.getOutputStream();
+    
+    
+    
+    
+	     /*   OutputStream os = conn.getOutputStream();
 	        byte[] bytes = file.getContent();
 	        os.write(bytes);
 	        os.flush();
@@ -150,6 +313,6 @@ public class FileUploadView {
     	System.out.println("sen video");    	
     }
  
-   
+   */
 
 }
